b8233359e32e7e51bd4828b113ec1484e8a72f25
Ashwin-Srinivasan <ashwins9999@gmail.com>
2018-07-31 13:45:34 -0400

    Added another observable generator for testing model gen

--- wc_kb_gen/random/__init__.py
+++ wc_kb_gen/random/__init__.py
@@ -1,6 +1,6 @@
-from .core import RandomKbGenerator
+from .core import RandomKbGenerator, RandomKbGenerator2
 from .genome import GenomeGenerator
 from .metabolites import MetabolitesGenerator
 from .properties import PropertiesGenerator
-from .observables import ObservablesGenerator
+from .observables import ObservablesGenerator, ObservablesGenerator2
 from .compartments import CompartmentsGenerator
--- wc_kb_gen/random/observables.py
+++ wc_kb_gen/random/observables.py
@@ -26,7 +26,8 @@ class ObservablesGenerator(wc_kb_gen.KbComponentGenerator):
         codons = [a + b + c for a in bases for b in bases for c in bases]
         default_trnas = []
         for codon in codons:
-            default_trnas.append('tRNA_'+codon)
+            if codon != 'TAA' and codon != 'TAG' and codon != 'TGA':
+                default_trnas.append('tRNA_'+codon)
 
         assigned_trnas = options.get('assigned_trnas', default_trnas)
 
@@ -91,3 +92,129 @@ class ObservablesGenerator(wc_kb_gen.KbComponentGenerator):
             observable.name = protein_name
             observable.species.append(
                 wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
+
+class ObservablesGenerator2(wc_kb_gen.KbComponentGenerator):
+    """
+    Creates observable objects for proteins and tRNAs that are assigned to specific functions. Adds these observables to the knowledge base.
+
+    Options:
+        * assigned_trnas (:obj:'list'): A list of the names of trnas to be created
+        * assigned_proteins (:obj: 'list'): A list of the names of proteins to be created
+    """
+
+    def clean_and_validate_options(self):
+        """ Apply default options and validate options """
+
+        options = self.options
+        bases = "TCAG"
+        codons = [a + b + c for a in bases for b in bases for c in bases]
+        default_trnas = []
+        for codon in codons:
+            default_trnas.append('tRNA_'+codon)
+
+        assigned_trnas = options.get('assigned_trnas', default_trnas)
+
+        options['assigned_trnas'] = assigned_trnas
+
+        assigned_proteins = options.get('assigned_proteins', ['IF1', 'IF2', 'IF3',
+                                                              'EF1', 'EF2', 'EF3',
+                                                              'RF',
+                                                              'deg_protease',
+                                                              'deg_rnase',
+                                                              'rna_poly_1','rna_poly_2', 'rna_poly_3'])
+
+        prots = self.knowledge_base.cell.species_types.get(
+            __type=wc_kb.ProteinSpeciesType)
+
+        assert(len(assigned_proteins) <= len(prots))
+        options['assigned_proteins'] = assigned_proteins
+
+    def gen_components(self):
+        """ Takes random samples of the generated rnas and proteins and assigns them functions based on the included list of proteins and rnas"""
+        cell = self.knowledge_base.cell
+        cytosol = cell.compartments.get_one(id='c')
+
+        assigned_trnas = self.options['assigned_trnas']
+        assigned_proteins = self.options['assigned_proteins']
+
+        prots = self.knowledge_base.cell.species_types.get(
+            __type=wc_kb.ProteinSpeciesType)
+        rnas = self.knowledge_base.cell.species_types.get(
+            __type=wc_kb.RnaSpeciesType)
+
+        trnas = []
+        for rna in rnas:
+            if rna.type == wc_kb.RnaType.tRna:
+                trnas.append(rna)
+
+        codons = {
+        'I': ['ATT', 'ATC', 'ATA'],
+        'L': ['CTT', 'CTC', 'CTA', 'CTG', 'TTA', 'TTG'],
+        'V': ['GTT', 'GTC', 'GTA', 'GTG'],
+        'F': ['TTT', 'TTC'],
+        'M': ['ATG'],
+        'C': ['TGT', 'TGC'],
+        'A': ['GCT', 'GCC', 'GCA', 'GCG'],
+        'G': ['GGT', 'GGC', 'GGA', 'GGG'],
+        'P': ['CCT', 'CCC', 'CCA', 'CCG'],
+        'T': ['ACT', 'ACC', 'ACA', 'ACG'],
+        'S': ['TCT', 'TCC', 'TCA', 'TCG', 'AGT', 'AGC'],
+        'Y': ['TAT', 'TAC'],
+        'W': ['TGG'],
+        'Q': ['CAA', 'CAG'],
+        'N': ['AAT', 'AAC'],
+        'H': ['CAT', 'CAC'],
+        'E': ['GAA', 'GAG'],
+        'D': ['GAT', 'GAC'],
+        'K': ['AAA', 'AAG'],
+        'R': ['CGT', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'],}
+
+        
+        for aa in codons:
+            rna = numpy.random.choice(trnas)
+            trnas.remove(rna)
+            species_coefficient = wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=rna, compartment=cytosol), coefficient=1)
+            for i in range(len(codons[aa])):
+                codon = codons[aa][i]
+                rna_name = 'tRNA_'+codon
+                observable = cell.observables.get_or_create(id=rna_name+'_obs')
+                observable.name = rna_name
+                observable.species.append(species_coefficient)
+                    
+
+        sampled_proteins = numpy.random.choice(
+            prots, len(assigned_proteins), replace=False)
+
+        assigned_proteins = iter(assigned_proteins)
+        for protein in sampled_proteins:
+            protein_name = next(assigned_proteins)
+            if protein_name.startswith('IF'):
+                observable = cell.observables.get_or_create(id='IF_obs')
+                observable.name = 'IF'
+                observable.species.append(
+                    wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
+            if protein_name.startswith('EF'):
+                observable = cell.observables.get_or_create(id='EF_obs')
+                observable.name = 'EF'
+                observable.species.append(
+                    wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
+            if protein_name.startswith('RF'):
+                observable = cell.observables.get_or_create(id='RF_obs')
+                observable.name = 'RF'
+                observable.species.append(
+                    wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
+            if protein_name.startswith('rna_poly'):
+                observable = cell.observables.get_or_create(id='rna_poly_obs')
+                observable.name = 'rna_poly'
+                observable.species.append(
+                    wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
+            if protein_name.startswith('deg_protease'):
+                observable = cell.observables.get_or_create(id='deg_protease_obs')
+                observable.name = 'deg_protease'
+                observable.species.append(
+                    wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
+            if protein_name.startswith('deg_rnase'):
+                observable = cell.observables.get_or_create(id='deg_rnase_obs')
+                observable.name = 'deg_rnase'
+                observable.species.append(
+                    wc_kb.SpeciesCoefficient(species=wc_kb.Species(species_type=protein, compartment=cytosol), coefficient=1))
