f48736e9e3d577e3bec9189af817760e71151527
Bilal Shaikh <bilalshaikh42@gmail.com>
2018-09-21 09:49:38 -0400

    Adding logging to the random package (__init__)

--- wc_kb_gen/random/__init__.py
+++ wc_kb_gen/random/__init__.py
@@ -5,3 +5,25 @@ from .properties import PropertiesGenerator
 from .observables import ObservablesGenerator
 from .compartments import CompartmentsGenerator
 from .complex import ComplexGenerator
+import logging
+from os import path, remove
+import sys
+
+if path.isfile("RandomKbGenerator.log"):
+    remove("RandomKbGenerator.log")
+root_logger = logging.getLogger(__name__)
+root_logger.setLevel(logging.DEBUG)
+
+root_logger_file_handler = logging.FileHandler("RandomKbGenerator.log")
+root_logger_file_handler.setLevel(logging.DEBUG)
+
+root_logger_output_handler = logging.StreamHandler(sys.stdout)
+root_logger_output_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+root_logger_file_handler.setFormatter(formatter)
+root_logger_output_handler.setFormatter(formatter)
+
+root_logger.addHandler(root_logger_file_handler)
+root_logger.addHandler(root_logger_output_handler)
