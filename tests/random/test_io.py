60751dc3d7ba3c4bccfc359c555b51d0283956a8
Jonathan Karr <jonrkarr@gmail.com>
2019-01-06 23:10:56 -0500

    updating for refactoring of wc_lang.io

--- tests/random/test_io.py
+++ tests/random/test_io.py
@@ -1,11 +1,12 @@
+from test.support import EnvironmentVarGuard
 from wc_model_gen import prokaryote
-import unittest
-import wc_kb_gen
-import wc_kb
-import unittest
 import os
 import shutil
 import tempfile
+import unittest
+import wc_kb
+import wc_kb_gen
+
 
 class KbIOTestCase(unittest.TestCase):
 
@@ -18,39 +19,38 @@ class KbIOTestCase(unittest.TestCase):
         shutil.rmtree(cls.dir)
 
     def test_generate_read_write(self):
-
         """ Generate KBs """
         kb_reduced = wc_kb_gen.random.RandomKbGenerator(options={
-                     'component': {
-                         'GenomeGenerator': {
-                             'genetic_code': 'reduced',
-                             'num_genes': 30,
-                             'num_tRNA': 4,
-                             'translation_table': 4,
-                             'seq_path': os.path.join(self.dir, 'kb_reduced_seq.fna')},
-                         'ObservablesGenerator': {
-                             'genetic_code': 'reduced'},
-                         }}).run()
+            'component': {
+                'GenomeGenerator': {
+                    'genetic_code': 'reduced',
+                    'num_genes': 30,
+                    'num_tRNA': 4,
+                    'translation_table': 4,
+                    'seq_path': os.path.join(self.dir, 'kb_reduced_seq.fna')},
+                'ObservablesGenerator': {
+                    'genetic_code': 'reduced'},
+            }}).run()
 
         kb = wc_kb_gen.random.RandomKbGenerator(options={
-                     'component': {
-                         'GenomeGenerator': {
-                             'num_genes': 100,
-                             'mean_gene_len': 70,
-                             'seq_path': os.path.join(self.dir, 'kb_seq.fna'),
-                             }}}).run()
+            'component': {
+                'GenomeGenerator': {
+                    'num_genes': 100,
+                    'mean_gene_len': 70,
+                    'seq_path': os.path.join(self.dir, 'kb_seq.fna'),
+                }}}).run()
 
         self.assertIsInstance(kb_reduced, wc_kb.core.KnowledgeBase)
         self.assertIsInstance(kb, wc_kb.core.KnowledgeBase)
 
         """ Write KBs to disk """
-        wc_kb.io.Writer().run(knowledge_base=kb_reduced,
-                              core_path=os.path.join(self.dir, 'kb_reduced.xlsx'),
+        wc_kb.io.Writer().run(os.path.join(self.dir, 'kb_reduced.xlsx'),
+                              kb_reduced,
                               set_repo_metadata_from_path=False)
 
-        wc_kb.io.Writer().run(knowledge_base=kb,
-                           core_path=os.path.join(self.dir, 'kb.xlsx'),
-                           set_repo_metadata_from_path=False)
+        wc_kb.io.Writer().run(os.path.join(self.dir, 'kb.xlsx'),
+                              kb,
+                              set_repo_metadata_from_path=False)
 
         self.assertTrue(os.path.isfile(os.path.join(self.dir, 'kb_reduced.xlsx')))
         self.assertTrue(os.path.isfile(os.path.join(self.dir, 'kb_reduced_seq.fna')))
@@ -58,12 +58,15 @@ class KbIOTestCase(unittest.TestCase):
         self.assertTrue(os.path.isfile(os.path.join(self.dir, 'kb_seq.fna')))
 
         """ Read back KBs from disk """
-        kb_reduced_read = wc_kb.io.Reader().run(core_path=os.path.join(self.dir, 'kb_reduced.xlsx'),
-                                                seq_path =os.path.join(self.dir, 'kb_reduced_seq.fna'),
-                                                strict=False)
-        kb_read = wc_kb.io.Reader().run(core_path=os.path.join(self.dir,'kb.xlsx'),
-                                        seq_path=os.path.join(self.dir,'kb_seq.fna'),
-                                        strict=False)
+        env = EnvironmentVarGuard()
+        env.set('CONFIG__DOT__wc_lang__DOT__io__DOT__strict', '0')
+        with env:
+            kb_reduced_read = wc_kb.io.Reader().run(core_path=os.path.join(self.dir, 'kb_reduced.xlsx'),
+                                                    seq_path=os.path.join(self.dir, 'kb_reduced_seq.fna')
+                                                    )[wc_kb.KnowledgeBase][0]
+            kb_read = wc_kb.io.Reader().run(core_path=os.path.join(self.dir, 'kb.xlsx'),
+                                            seq_path=os.path.join(self.dir, 'kb_seq.fna'),
+                                            )[wc_kb.KnowledgeBase][0]
 
         self.assertIsInstance(kb_reduced_read, wc_kb.core.KnowledgeBase)
         self.assertIsInstance(kb_read, wc_kb.core.KnowledgeBase)
